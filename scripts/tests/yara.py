e724dcfef3b0c815bbbfe173b1f53a4a9a5c532a
Dylane Bengono <chaneldylanebengono@gmail.com>
2024-09-05 10:20:27 +0100

    chore: Ensure yara_rules.yar file exists and has correct ownership

--- scripts/tests/yara.py
+++ scripts/tests/yara.py
@@ -51,11 +51,14 @@ def test_wazuh_agent_restarted(host):
     assert service.is_running
     assert service.is_enabled
 
+
 def test_yara_rules_downloaded(host):
     yara_rules_dir = "/var/ossec/ruleset/yara/rules"
     yara_rules_file = os.path.join(yara_rules_dir, "yara_rules.yar")
     
     file = host.file(yara_rules_file)
-    assert file.exists
-    assert file.user == "root"
-    assert file.group == "wazuh"
+    
+    assert file.exists(), f"{yara_rules_file} does not exist"
+    assert file.user == "root", f"File is not owned by 'root', but by {file.user}"
+    assert file.group == "wazuh", f"File group is not 'wazuh', but {file.group}"
+
