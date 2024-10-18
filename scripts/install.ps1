4a28d930528d563f62e8f1d313bf166d7d11f9c0
MarantosGeorge <marantosgeorge@gmail.com>
2024-10-18 13:18:15 +0200

    Update: removed dependecy installation from install.ps1 and added yara.bat installation

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -44,59 +44,6 @@ function Ensure-Admin {
     }
 }
 
-# Function to check if Python is installed and has the correct version
-function Check-PythonInstalled {
-    try {
-        $pythonVersion = & python --version 2>&1
-        if ($pythonVersion -match "Python (\d+)\.(\d+)\.(\d+)") {
-            $majorVersion = [int]$matches[1]
-            $minorVersion = [int]$matches[2]
-            $patchVersion = [int]$matches[3]
-            
-            if ($majorVersion -ge 3 -and $minorVersion -ge 9) {
-                Write-Host "Python $majorVersion.$minorVersion.$patchVersion is installed and is a recent version." -ForegroundColor Green
-                return $true
-            } else {
-                Write-Host "Python version is $majorVersion.$minorVersion.$patchVersion. Please install Python 3.9 or later and run the script again." -ForegroundColor Red
-                exit
-            }
-        } else {
-            throw "Python is not installed or not properly configured."
-        }
-    } catch {
-        Write-Host "Python is not installed or not properly configured. Installing Python..." -ForegroundColor Yellow
-        Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe" -OutFile "$env:TEMP\python-3.9.0-amd64.exe"
-        Start-Process -FilePath "$env:TEMP\python-3.9.0-amd64.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
-        Remove-Item -Path "$env:TEMP\python-3.9.0-amd64.exe"
-
-        # Update environment variables
-        [System.Environment]::SetEnvironmentVariable("Path", [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";C:\Program Files\Python39", "Process")
-
-        # Update pip to the latest version
-        try {
-            & python -m pip install --upgrade pip
-        } catch {
-            Write-Error "Failed to update pip: $_"
-            exit 1
-        }
-    }
-}
-
-# Function to check if Visual C++ Redistributable is installed
-function Check-VCppInstalled {
-    $vcppKey = "HKLM:\SOFTWARE\Microsoft\VisualStudio\14.0\VC\Runtimes\x64"
-    if (Test-Path $vcppKey) {
-        $vcppInstalled = Get-ItemProperty -Path $vcppKey
-        if ($vcppInstalled -and $vcppInstalled.Installed -eq 1) {
-            Write-Host "Visual C++ Redistributable is installed." -ForegroundColor Green
-            return $true
-        }
-    }
-    Write-Host "Visual C++ Redistributable is not installed. Installing Visual C++ Redistributable..." -ForegroundColor Yellow
-    Invoke-WebRequest -Uri "https://aka.ms/vs/16/release/vc_redist.x64.exe" -OutFile "$env:TEMP\vc_redist.x64.exe"
-    Start-Process -FilePath "$env:TEMP\vc_redist.x64.exe" -ArgumentList "/quiet /install" -Wait
-    Remove-Item -Path "$env:TEMP\vc_redist.x64.exe"
-}
 
 # Function to download and extract YARA
 function Download-YARA {
@@ -174,58 +121,12 @@ if (Test-Path -Path $yaraRulesPath) {
     exit 1
 }
 
-    # Create the yara.bat script
-    $yaraBatContent = @"
-@echo off
+    
+    # Download yara.bat script
+    $yaraBatURL = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-yara/refs/heads/3-Windows-Agent-Install-Script/scripts/yara.bat"
 
-setlocal enableDelayedExpansion
-
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && SET OS=32BIT || SET OS=64BIT
-
-if %OS%==32BIT (
-    SET log_file_path="%programfiles%\ossec-agent\active-response\active-responses.log"
-)
-
-if %OS%==64BIT (
-    SET log_file_path="%programfiles(x86)%\ossec-agent\active-response\active-responses.log"
-)
-
-set input=
-for /f "delims=" %%a in ('PowerShell -command "$logInput = Read-Host; Write-Output $logInput"') do (
-    set input=%%a
-)
-
-set json_file_path="C:\Program Files (x86)\ossec-agent\active-response\stdin.txt"
-set syscheck_file_path=
-echo %input% > %json_file_path%
-
-for /F "tokens=* USEBACKQ" %%F in (`Powershell -Nop -C "(Get-Content 'C:\Program Files (x86)\ossec-agent\active-response\stdin.txt'|ConvertFrom-Json).parameters.alert.syscheck.path"`) do (
-    set syscheck_file_path=%%F
-    echo DEBUG: syscheck_file_path=!syscheck_file_path! >> %log_file_path%
-)
-
-del /f %json_file_path%
-set yara_exe_path="C:\Program Files (x86)\ossec-agent\active-response\bin\yara\yara64.exe"
-set yara_rules_path="C:\Program Files (x86)\ossec-agent\active-response\bin\yara\rules\yara_rules.yar"
-echo !syscheck_file_path! >> %log_file_path%
-
-set malware_detected=false
-for /f "delims=" %%a in ('powershell -command "& \"%yara_exe_path%\" \"%yara_rules_path%\" \"%syscheck_file_path%\""') do (
-    echo wazuh-yara: INFO - Scan result: %%a >> %log_file_path%
-    if "%%a" NEQ "0" (
-        set malware_detected=true
-        echo DEBUG: malware_detected=!malware_detected! >> %log_file_path%
-    )
-)
-
-if !malware_detected! == true (
-    del /f !syscheck_file_path!
-    echo wazuh-yara: INFO - Malware file !syscheck_file_path! removed >> %log_file_path%
-)
-
-exit /b
-"@
-    $yaraBatContent | Out-File -FilePath "C:\Program Files (x86)\ossec-agent\active-response\bin\yara.bat" -Encoding utf8
+    Invoke-WebRequest -Uri $yaraBatUrl -FilePath "C:\Program Files (x86)\ossec-agent\active-response\bin\yara.bat" 
+    
 
     # Update Wazuh agent configuration
     Update-WazuhConfig
