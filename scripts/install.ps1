df8b268c33309fb095593104521189f00f99ec92
MarantosGeorge <marantosgeorge@gmail.com>
2024-10-21 00:25:53 +0200

    Update: Moved yara.bat script to seperate file

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -123,58 +123,14 @@ if (Test-Path -Path $yaraRulesPath) {
     exit 1
 }
 
-    # Create the yara.bat script
-    $yaraBatContent = @"
-@echo off
-
-setlocal enableDelayedExpansion
-
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && SET OS=32BIT || SET OS=64BIT
-
-if %OS%==32BIT (
-    SET log_file_path="%programfiles%\ossec-agent\active-response\active-responses.log"
-)
-
-if %OS%==64BIT (
-    SET log_file_path="%programfiles(x86)%\ossec-agent\active-response\active-responses.log"
-)
-
-set input=
-for /f "delims=" %%a in ('PowerShell -command "$logInput = Read-Host; Write-Output $logInput"') do (
-    set input=%%a
-)
-
-set json_file_path="C:\Program Files (x86)\ossec-agent\active-response\stdin.txt"
-set syscheck_file_path=
-echo %input% > %json_file_path%
-
-for /F "tokens=* USEBACKQ" %%F in (`Powershell -Nop -C "(Get-Content 'C:\Program Files (x86)\ossec-agent\active-response\stdin.txt'|ConvertFrom-Json).parameters.alert.syscheck.path"`) do (
-    set syscheck_file_path=%%F
-    echo DEBUG: syscheck_file_path=!syscheck_file_path! >> %log_file_path%
-)
-
-del /f %json_file_path%
-set yara_exe_path="C:\Program Files (x86)\ossec-agent\active-response\bin\yara\yara64.exe"
-set yara_rules_path="C:\Program Files (x86)\ossec-agent\active-response\bin\yara\rules\yara_rules.yar"
-echo !syscheck_file_path! >> %log_file_path%
-
-set malware_detected=false
-for /f "delims=" %%a in ('powershell -command "& \"%yara_exe_path%\" \"%yara_rules_path%\" \"%syscheck_file_path%\""') do (
-    echo wazuh-yara: INFO - Scan result: %%a >> %log_file_path%
-    if "%%a" NEQ "0" (
-        set malware_detected=true
-        echo DEBUG: malware_detected=!malware_detected! >> %log_file_path%
-    )
-)
-
-if !malware_detected! == true (
-    del /f !syscheck_file_path!
-    echo wazuh-yara: INFO - Malware file !syscheck_file_path! removed >> %log_file_path%
-)
-
-exit /b
-"@
-    $yaraBatContent | Out-File -FilePath "C:\Program Files (x86)\ossec-agent\active-response\bin\yara.bat" -Encoding utf8
+    #Download the yara.bat script
+    $yaraBatURL = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-yara/refs/heads/3-Windows-Agent-Install-Script/scripts/yara.bat"
+    $yaraBatDir =  "C:\Program Files (x86)\ossec-agent\active-response\bin\yara.bat"
+    
+    
+    # Download the appropriate YARA version
+    Invoke-WebRequest -Uri $yaraUrl -OutFile $yaraBatDir
+    Write-Host "Yara Bat Script Downloaded and copied into $yaraBatDir "
 
     # Update Wazuh agent configuration
     Update-WazuhConfig
