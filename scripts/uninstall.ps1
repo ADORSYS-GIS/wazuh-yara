e992ad6ea8ff39f8df964a37bc549c20ac58dc0a
MarantosGeorge <marantosgeorge@gmail.com>
2025-01-24 18:02:51 +0200

    feat:Update uninstall script
    - Add: YARA batch file removal
    - Add: YARA Path removal from environment variables
    - Add: Removal of ossec configuration for Yara

--- scripts/uninstall.ps1
+++ scripts/uninstall.ps1
@@ -1,7 +1,7 @@
-$AgentVersion = "4.9.2-1"
-$OssecPath = "C:\Program Files (x86)\ossec-agent"
-$DownloadUrl = "https://packages.wazuh.com/4.x/windows/wazuh-agent-$AgentVersion.msi"
-$TempFile = New-TemporaryFile
+$agentVersion = "4.9.2-1"
+$ossecPath = "C:\Program Files (x86)\ossec-agent"
+$downloadUrl = "https://packages.wazuh.com/4.x/windows/wazuh-agent-$AgentVersion.msi"
+$tempFile = New-TemporaryFile
 
 
 function Log {
@@ -64,28 +64,83 @@ function Restart-WazuhAgent {
 
 #Uninstall Yara
 function Uninstall-Yara {
-    $YaraDir = "C:\Program Files (x86)\ossec-agent\active-response\bin\yara"
-    InfoMessage "Removing Yara..."
-    try {
-        Remove-Item -Path $YaraDir -Recurse -Force
-        InfoMessage "Yara executable removed succesfully"
+    $yaraDir = "C:\Program Files (x86)\ossec-agent\active-response\bin\yara"
+    $yaraBatFile = "C:\Program Files (x86)\ossec-agent\active-response\bin\yara.bat"
+    $currentPath = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
+    InfoMessage "Removing YARA..."
+    if (Test-Path -Path $yaraDir) {
+        Remove-Item -Path $yaraDir -Force -Recurse -ErrorAction SilentlyContinue
+        InfoMessage "YARA directory removed: $yaraDir"
+    } else {
+        WarnMessage "YARA directory not found: $yaraDir. Skipping..."
     }
-    catch {
-        WarnMessage "Yara executable is not installed. Skipping uninstallation"
-    }
-}
-
-function Remove-YaraPath {
     
 
+    if (Test-Path -Path $yaraBatFile) {
+        Remove-Item -Path $yaraBatFile -Force -ErrorAction SilentlyContinue
+        InfoMessage "YARA batch file removed: $yaraBatFile"
+    } else {
+        WarnMessage "YARA batch file not found: $yaraBatFile. Skipping..."
+    }
+
+    #Remove YARA path from environment variables
+    if ($currentPath -like "*$yaraDir*") {
+        $newPath = $currentPath -replace [Regex]::Escape(";$yaraPath"), ""
+        [System.Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
+        Log "INFO" "YARA path removed from environment variables"
+    } else {
+        Log "INFO" "YARA path not found in environment variables"
+    }
 }
-
-
-#Remove YARA rules and scripts
-
 
 
 
 #Remove ossec configuration modifications
+function Remove-OssecConfigurations {
+    InfoMessage "Removing OSSEC configuration modifications..."
 
+    $configFilePath = "C:\Program Files (x86)\ossec-agent\ossec.conf"
+    if (Test-Path -Path $configFilePath) {
+        [xml]$configXml = Get-Content -Path $configFilePath
 
+        $syscheckNode = $configXml.ossec_config.syscheck
+        if ($null -ne $syscheckNode) {
+            # Remove any added directories
+            $downloadDirNode = $syscheckNode.directories | Where-Object { $_.InnerText -eq "C:\Users\$([System.Environment]::UserName)\Downloads" }
+            if ($null -ne $downloadDirNode) {
+                $syscheckNode.RemoveChild($downloadDirNode) | Out-Null
+                InfoMessage "Removed syscheck directory: C:\Users\$([System.Environment]::UserName)\Downloads"
+            }
+
+            # Remove <file_limit> if it exists
+            $fileLimitNode = $syscheckNode.file_limit
+            if ($null -ne $fileLimitNode) {
+                $syscheckNode.RemoveChild($fileLimitNode) | Out-Null
+                InfoMessage "Removed file_limit from syscheck configuration"
+            }
+
+            $configXml.Save($configFilePath)
+            Restart-Service -Name WazuhSvc -ErrorAction SilentlyContinue
+            InfoMessage "Wazuh configuration restored and service restarted"
+        } else {
+            WarnMessage "No <syscheck> node found in Wazuh configuration"
+        }
+    } else {
+        WarnMessage "Wazuh configuration file not found: $configFilePath"
+    }
+}
+
+#Main Uninstall Function
+function Uninstall-All {
+    try {
+        Uninstall-Yara
+        Remove-OssecConfigurations
+        Restart-WazuhAgent
+        SuccessMessage "YARA and components uninstalled successfully"
+    }
+    catch {
+        ErrorMessage "YARA Uninstall Failed: $($_.Exception.Message)"
+    }
+}
+
+Uninstall-All
